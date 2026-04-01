7bf0dc11bfc018fa03b292f9a42be2e3826fa35a
Givi Koyava <sohumelivar@gmail.com>
2026-04-01 19:09:51 +0300

    feat: implement order card layout and styles

--- client/src/components/OrderCard/OrderCard.tsx
+++ client/src/components/OrderCard/OrderCard.tsx
@@ -1,14 +1,70 @@
 import type { OrderType } from '../../types/order.types';
 import './OrderCard.css';
+import MessageSquare from '../../assets/icons/message-square-more.svg?react';
+import Pencil from '../../assets/icons/pencil.svg?react';
+import Trash from '../../assets/icons/trash-2.svg?react';
 
 type Props = {
   order: OrderType;
 };
 
 export const OrderCard = ({ order }: Props) => {
+    const hasMessage = Boolean(order.comment);
+    const orderStatus = order.status;
+    const formattedDate = new Date(order.created_at)
+        .toISOString()
+        .split('T')[0];
+
     return (
-        <div className='order-card'>
-            
+        <div className={`order-card order-card--${order.status}`}>
+            <div className="order-card__header">
+                <div className="order-card__sizes">
+                    <div className="order-card__size">
+                        <span className="label">SUCTION</span>
+                        <span className="value">{order.suction_size}</span>
+                    </div>
+                    <div className="divider" />
+                    <div className="order-card__size">
+                        <span className="label">LIQUID</span>
+                        <span className="value">{order.liquid_size}</span>
+                    </div>
+                </div>
+                <div className="order-card__actions">
+                    <MessageSquare 
+                        className={`action-icon ${hasMessage ? 'active' : 'disabled'}`}
+                    />
+                    <Pencil 
+                        className={`action-icon ${orderStatus === 'waiting' ? 'active' : 'disabled'}`}
+                    />
+                    <Trash 
+                        className={`action-icon ${orderStatus === 'waiting' ? 'active' : 'disabled'}`}
+                    />
+                </div>
+            </div>
+            <div className="order-card-divider"/>
+            <div className="order-card__body">
+                <div className="length_quantity_labels">
+                    <div className="length_label">LENGTH</div>
+                    <div className="quantity_label">QUANTITY</div>
+                </div>
+                <div className="length_quantity_values">
+                    <div className="length_value">{order.length} m</div>
+                    <div className="quantity_value">{order.quantity} pcs</div>
+                </div>
+            </div>
+            <div className="order-card-divider"/>
+            <div className="order-card__footer">
+                <div className="total_created_labels">
+                    <div className="total_label">TOTAL</div>
+                    <div className="created_label">CREATED</div>
+                </div>
+                <div className="total_created_values">
+                    <div className="total_value">{order.quantity * order.length} m</div>
+                    <div className="created_value">{formattedDate}</div>
+                </div>
+            </div>
+            <div className="order-card-divider"/>
+            <div className="order-card__button"></div>
         </div>
     );
 };
--- client/src/pages/ActiveOrders/ActiveOrders.tsx
+++ client/src/pages/ActiveOrders/ActiveOrders.tsx
@@ -1,4 +1,24 @@
+import { OrderCard } from "../../components/OrderCard/OrderCard";
+
 export const ActiveOrdersPage = () => {
-    
-    return <div>Active Orders</div>;
+    const order = {
+        id: 1,
+        suction_size: "3/4",
+        liquid_size: "3/8",
+        length: 10,
+        quantity: 5,
+        status: "waiting",
+        created_at: "2026-03-29T20:05:27.204Z",
+        comment: "test comment",
+        updated_at: "2026-03-29T20:05:27.204Z",
+        completed_at: null
+    }
+    return (
+        <div>
+            <div>
+                <OrderCard order={order}/>
+            </div>
+
+        </div>
+    );
 };
