3acea44453375224f6448fb6594e99969290fbaf
Givi Koyava <sohumelivar@gmail.com>
2026-04-02 23:58:05 +0300

    9

--- client/src/utils/getInitData.ts
+++ client/src/utils/getInitData.ts
@@ -1,10 +1,9 @@
 export const getInitData = (): string => {
-  const initData = window.Telegram?.WebApp?.initData;
-  console.log('initData', initData);
-  
-  if (initData) {
-    return initData;
-  }
+	const initData = window.Telegram?.WebApp?.initData;
 
-  return import.meta.env.VITE_DEV_INIT_DATA ?? '';
+	if (initData) {
+	return initData;
+	}
+
+	return import.meta.env.VITE_DEV_INIT_DATA ?? '';
 };
