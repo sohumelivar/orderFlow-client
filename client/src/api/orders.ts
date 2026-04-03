f924e1a7a7ac1537e0bfb9e52ff3037a2b9490f4
Givi Koyava <sohumelivar@gmail.com>
2026-04-03 19:33:01 +0300

    18

--- client/src/api/orders.ts
+++ client/src/api/orders.ts
@@ -2,6 +2,6 @@ import { api } from './axios';
 import type { ActiveOrdersResponse } from '../types/order.types';
 
 export const getActiveOrders = async (): Promise<ActiveOrdersResponse> => {
-    const response = await api.get('http://95.217.2.56:3001/api/orders/active');
+    const response = await api.get('/orders/active');
     return response.data;
 };
